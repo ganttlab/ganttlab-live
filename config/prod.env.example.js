d8bbc9715129897025f14a4e4c1517c108c1b34e
clorichel <contact@clorichel.com>
2016-10-23 14:38:23 +0100

    prod.env.js should not be committed

---
+++ config/prod.env.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  GITLAB_URL: '"https://gitlab.com"',
+  GITLAB_PRIVATE_TOKEN: '"place your GitLab private token here"',
+  MOMENTJS_LOCALE: '"en"'
+}
