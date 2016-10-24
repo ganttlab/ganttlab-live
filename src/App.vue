707b829faf2237ca293a101563cecb0ca982ef0b
clorichel <contact@clorichel.com>
2016-10-24 23:19:03 +0100

    analytics on live running https://clorichel.gitlab.io/ganttlab/

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,16 @@ export default {
     if (this.url && this.token) {
       this.init()
     }
+    if (process.env.NODE_ENV === 'production') {
+      /* eslint-disable */
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-78984168-3', 'auto');
+      ga('send', 'pageview');
+      /* eslint-enable */
+    }
   }
 }
 </script>
