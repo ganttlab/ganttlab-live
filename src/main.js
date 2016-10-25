7ff8b5128c5a420de33fe5e5efc44947768554fe
clorichel <contact@clorichel.com>
2016-10-25 22:29:15 +0100

    using vue-gitlab-api as the GitLabAPI wrapper

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
 
+import GitLabAPI from 'vue-gitlab-api'
+Vue.use(GitLabAPI)
+
 import App from './App'
 
 /* eslint-disable no-new */
